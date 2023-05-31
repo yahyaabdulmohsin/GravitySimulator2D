5a83cd26e32ec162d96263fd50c8694b030c16e4
Romain <92953283+R-Besson@users.noreply.github.com>
2023-05-31 22:15:50 +0200

    Update CelestialBody.cpp
    You don't need to do all those ifs! All you need to do is break it down into x and y components...

--- CelestialBody.cpp
+++ CelestialBody.cpp
@@ -21,52 +21,8 @@ CelestialBody::CelestialBody(float pos_x, float pos_y, float radius, float mass,
         s.setFillColor(sf::Color::Blue);
     }
 
-    float angleRADS = (3.1415926536 / 180) * (direction);
-    if (direction == 0)
-    {
-        xVelocity = velocity;
-        yVelocity = 0.f;
-    }
-    else if (direction < 90)
-    {
-        xVelocity = velocity * std::cos(angleRADS);
-        yVelocity = velocity * std::sin(angleRADS);
-    }
-    else if (direction == 90)
-    {
-        xVelocity = 0.f;
-        yVelocity = velocity;
-    }
-    else if (direction < 180)
-    {
-        xVelocity = -1 * velocity * std::cos(3.1415926536 - angleRADS);
-        yVelocity = velocity * std::sin(3.1415926536 - angleRADS);
-    }
-    else if (direction == 180)
-    {
-        xVelocity = -1 * velocity;
-        yVelocity = 0.f;
-    }
-    else if (direction < 270)
-    {
-        xVelocity = -1 * velocity * std::sin(((3 * 3.1415926536) / 2) - angleRADS);
-        yVelocity = -1 * velocity * std::cos(((3 * 3.1415926536) / 2) - angleRADS);
-    }
-    else if (direction == 270)
-    {
-        xVelocity = 0.f;
-        yVelocity = -1 * velocity;
-    }
-    else if (direction < 360)
-    {
-        xVelocity = velocity * std::cos((2 * 3.1415926536) - angleRADS);
-        yVelocity = -1 * velocity * std::sin((2 * 3.1415926536) - angleRADS);
-    }
-    else if (direction == 360)
-    {
-        xVelocity = velocity;
-        yVelocity = 0.f;
-    }
+    xVelocity = velocity * std::cos(angleRADS);
+    yVelocity = velocity * std::sin(angleRADS);
 }
 
 void CelestialBody::render(sf::RenderWindow &wind)
